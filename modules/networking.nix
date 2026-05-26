f621fe8e9d5d4fb3c578f79cbc6ac6accadfda0f
endotrizine <starnicworld@gmail.com>
2026-05-26 06:34:43 +0500

    update

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,4 +2,17 @@
 {
   # hostName is set per-host in hosts/<name>/default.nix.
   networking.networkmanager.enable = true;
+	programs.clash-verge = {
+		enable = true;
+		serviceMode = true;
+		tunMode = true;
+		autoStart = true;
+	};
+
+	networking.firewall = {
+		trustedInterfaces = [ "Mihomo" ];
+		extraReversePathFilterRules= ''
+			iifname { "Mihomo" } accept comment "trusted interface"
+		'';
+	};
 }
--- modules/services.nix
+++ modules/services.nix
@@ -3,7 +3,6 @@
   services.openssh.enable = true;
   services.dbus.enable = true;
   services.upower.enable = true;
-
   security.polkit.enable = true;
 
   hardware.bluetooth.enable = true;
